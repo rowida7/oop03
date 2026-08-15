b1a63c5bcc5e0b6ca06a14007d0db89826eed094
rowida7 <rowidaabdelfatah@gmail.com>
2026-08-15 21:01:02 +0300

    Create DeliveryHelper

---
+++ DeliveryHelper.cs
@@ -0,0 +1,15 @@
+﻿using oop02;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop03
+{
+    internal static class DeliveryHelper
+    {
+        public static void PrintShipmentDetails(Shipment shipment)
+        {
+            shipment.PrintShipment();
+        }
+    }
+}
