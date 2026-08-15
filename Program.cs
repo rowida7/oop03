dbb2a3b212212a117b4805e60525f84e17bb3196
rowida7 <rowidaabdelfatah@gmail.com>
2026-08-15 20:03:08 +0300

    Theoretical Questions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Theoretical Questions
+            //Q1  Overloading, Overriding, and Binding
+                //a)  What is the difference between Method Overloading and Method Overriding?
+                // Method Overloading having many methods with the same name and different parameter (order, count, dataType)
+                // Method Overriding a virtual method that overrided in the inherited class to have different behaviour
+
+
+                //b)  What is the difference between Static Binding and Dynamic Binding?
+                // Static Binding is deciding the method to call in Compile Time
+                // Dynamic Binding is deciding the method to call in RunTime
+
+            //Q2 Sealed Classes and Methods
+                //a)  What is the purpose of the sealed keyword when applied to a class?
+                // that class can't be inherited
+
+                //b)  What is the difference between a sealed class and a sealed method?
+                // sealed class can't be inherited 
+                // sealed method can't be overrided
+
+                //c)  Can a sealed method be overridden? Why?
+                // No, because it's stoping the overriding chain
+            #endregion
+
+         
         }
     }
 }
